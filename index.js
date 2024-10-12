789b478da216815a0f985215e59829eaefae5de1
Angel Saavedra <angelmsaavedra@icloud.com>
2024-10-12 16:09:36 +0200

    Basic UI and initial object definition

---
+++ index.js
@@ -0,0 +1,55 @@
+function createGameboard() {
+    return {
+        tiles: [],
+    }
+}
+
+function createPlayer(name, team) {
+    return  {
+        name: name,  
+        team: team,
+    }
+}
+
+function createGame() {
+    const start = TODO;
+    const players = [];
+    let round = "x";
+    
+    startGame();
+
+    function startGame() {
+
+        // TODO: ask user for input for name
+
+        const player1 = createPlayer(name1, "x");
+        const player2 = createPlayer(name2, "o");
+
+        players.push(player1, player2);
+    }
+
+    function startRound() {
+        if (round === "x") {
+
+            round = "o";
+        } else {
+
+            round = "x";
+        }
+    }
+}
+
+let game = createGame();
+game.start();
+
+const button = document.querySelector("#start");
+const gameboard = document.querySelector(".gameboard");
+
+const gameboardListener = gameboard.addEventListener("click", handleClick);
+
+function handleClick() {
+
+
+    
+}
+
